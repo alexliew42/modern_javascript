18760135435d8dffedf7c3be024f96929c478d21
Alex Liew <liewtwoo42@gmail.com>
2023-05-10 13:36:38 -0400

    add dat varaible

--- index.js
+++ index.js
@@ -3,3 +3,6 @@
 console.log("I am in the index.js file");
 var datetime = moment().startOf("day").fromNow();
 console.log(datetime);
+
+var dat = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(dat);
