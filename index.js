a34a3af494d6ac6631eb6d7bca6ee2fc82e5a1e8
Alex Liew <liewtwoo42@gmail.com>
2023-05-10 15:17:18 -0400

    install live-server

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ var slugify = require("underscore.string/slugify");
 
 
 
-
+var bob = {
+  _name: "Bob",
+  _friends: [],
+  printFriends() {
+    this._friends.forEach(f =>
+      console.log(this._name + " knows " + f));
+  }
+};
+console.log(bob.printFriends);
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 console.log(simplify('2 * 3 * x', { x: 4 }).toString());
 console.log("change is hapeening");
 console.log(slugify("Hello World This is working."));
+console.log("the live server us working");
 
 
 
