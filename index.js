83014e56d0c6420de6a00b43d50a5b061a3681b6
Alex Liew <liewtwoo42@gmail.com>
2023-05-10 14:12:17 -0400

    implement underscore string with slugify variable

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const moment = require('moment');
 const { sqrt } = require('mathjs');
 const { simplify, parse, derivative } = require('mathjs');
+var slugify = require("underscore.string/slugify");
+
+
 
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 console.log(simplify('2 * 3 * x', { x: 4 }).toString());
 console.log("change is hapeening");
+console.log(slugify("Hello World This is working."));
 
 
 
